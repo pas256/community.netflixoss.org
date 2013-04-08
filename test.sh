9bb9e52e06f2491591f9cc3d2bf7f6038af9d202
Peter Sankauskas <peter@admobius.com>
2013-04-07 23:49:50 -0700

    Ignoring the test site

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#bfdocs --server --port=3001 --watch --base-url=/ --templates-dir=templates/community src/manifest.json html
-bfdocs --server --port=3001 --watch --base-url=/ src/manifest.json html
+#bfdocs --server --port=3001 --watch --base-url=/ --templates-dir=templates/community src/manifest.json site
+bfdocs --server --port=3001 --watch --base-url=/ src/manifest.json site
