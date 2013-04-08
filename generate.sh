923e234958d70736a0d2cf63373b81a59fe5bb11
Peter Sankauskas <peter@admobius.com>
2013-04-07 23:48:16 -0700

    Updating generate/test scripts to use the default template so the necessary CSS files are downloaded

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bfdocs --base-url=/ --templates-dir=templates/community src/manifest.json ../community.netflixoss.org-html
+#bfdocs --base-url=/ --templates-dir=templates/community src/manifest.json ../community.netflixoss.org-html
+bfdocs --base-url=/ src/manifest.json ../community.netflixoss.org-html
